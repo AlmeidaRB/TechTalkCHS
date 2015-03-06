253d708254097af251bdf16b708f48666d8afbaa
raveve <raven.askew@gmail.com>
2015-03-06 15:29:34 -0500

    Commented out Calendar Service and Controller code to resolve errors.

--- calendar/calendar.service.js
+++ calendar/calendar.service.js
@@ -9,27 +9,27 @@
 
 })();
 
-(function (){
-    "use strict";
-    angular.module('.ttcApp')
-    .factory('CalendarService', function($http, $rootScope){
-         var url: "http://www.tiy-fee-rest.herokuapp.com/collections/ttcService"
-
-
-          var getEvent = function () {
-            return $http.get(url);
-          };
-          var addEvent = function (Info) {
-            $http.post(url, Info);
-          };
-
-
-        return {
-          getEvent: getEvent,
-          addEvent: addEvent,
-
-        };
-
-        })
-
-    })
+// (function (){
+//     "use strict";
+//     angular.module('.ttcApp')
+//     .factory('CalendarService', function($http, $rootScope){
+//          var url: "http://www.tiy-fee-rest.herokuapp.com/collections/ttcService"
+//
+//
+//           var getEvent = function () {
+//             return $http.get(url);
+//           };
+//           var addEvent = function (Info) {
+//             $http.post(url, Info);
+//           };
+//
+//
+//         return {
+//           getEvent: getEvent,
+//           addEvent: addEvent,
+//
+//         };
+//
+//         })
+//
+//     })
